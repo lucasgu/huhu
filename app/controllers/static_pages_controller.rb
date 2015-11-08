33b3083a26c6ae33e44ed9b456bb21de50651d4b
wenjin wu <wenjin@wuwenjindeMacBook-Pro.local>
2015-11-08 14:46:41 +0000

    Finish user signup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,5 +5,6 @@ class StaticPagesController < ApplicationController
   def help
   end
   def about
+  	
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,22 @@ class UsersController < ApplicationController
   def show
         @user = User.find(params[:id])
   end
+     def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+        flash[:success] = "Welcome to the Twitter App!"    # NEW LINE
+          redirect_to @user   # NEW LINE
+ # Handle a successful save.
+        else
+          render 'new'
+  # Handle an unsuccessful save.     
+        end
+      end
 
 
   def new
+  	 @user = User.new
   end
 end
