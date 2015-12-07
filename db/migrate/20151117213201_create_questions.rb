c4d97e3954896e48043a5ad63ad0052de94813b9
wenjin wu <wenjin@wuwenjindeMacBook-Pro.local>
2015-12-07 08:33:57 +0000

    stop write the topicpart

--- db/migrate/20151117213201_create_questions.rb
+++ db/migrate/20151117213201_create_questions.rb
@@ -3,7 +3,6 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string :name
       t.text :content
-      
       t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
--- db/migrate/20151206204816_create_question_topic_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateQuestionTopicJoinTable < ActiveRecord::Migration
-  def change
-  	create_table :questions_topics, :id => false do |t|
-          t.integer :topic_id
-          t.integer :question_id
-  end
-end
-end
