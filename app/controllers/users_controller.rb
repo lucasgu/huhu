e1cb39ee0e5d220e58be664161b04328949b6695
wenjin wu <wenjin@wuwenjindeMacBook-Pro.local>
2015-12-07 09:31:34 +0000

    finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ before_action :logged_in_user, only: [:index]#new in 11 14
         @user = User.new(secure_params)
         if @user.save
          remember @user       #  NEW LINE
-
         flash[:success] = "Welcome to the Twitter App!"    # NEW LINE
           redirect_to @user   # NEW LINE
  # Handle a successful save.
@@ -33,12 +32,28 @@ before_action :logged_in_user, only: [:index]#new in 11 14
           @users=User.all
         end #2015 11 14 new line
 
+def edit 
+  @user=User.find(params[:id])
+end
 
 
+def update
+@user = User.find(params[:id])
+if @user.update_attributes(user_params)
+   flash[:success] = "Profile updated"
+redirect_to @user
 
+else
+render 'edit' 
+end
 
+end
 
-
+private
+def user_params
+params.require(:user).permit(:name, :email, :password,
+                                       :password_confirmation)
+end
 
 
 
