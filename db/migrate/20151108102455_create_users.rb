0e793b4d79b200572ca66c6454f616a48955b478
wenjin wu <wenjin@wuwenjindeMacBook-Pro.local>
2015-12-08 19:34:37 +0000

    add some seeds

--- db/migrate/20151108102455_create_users.rb
+++ db/migrate/20151108102455_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      validates :topic_id
 
 
       t.timestamps null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,37 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Create some users
     
+
+
+    #MICROPOST
  # Associate some microposts with users
- 
+   User.create(name:'Justin', email:'ssswwj110@gmail.com', password: "ssswwj110", password_confirmation: "ssswwj110")
+  User.create(name:'Alex', email:'jc@here.com', password: "secret", password_confirmation: "secret")
+    # Associate some microposts with users
+  User.create(name:'Tom', email:'ssswwj111@gmail.com', password: "secret", password_confirmation: "secret")
+
+    user = User.find(1)
+    user.microposts.create(content: "Ruby is the best")
+    user.microposts.create(content: "Changed my mind, it's Java")
+    user = User.find(2)
+    user.microposts.create(content: "What Ruby lacks is ....")
+    
+    # Associate some tags with microposts
+#TOPIC PART  
+
+    java=Topic.create(name: "Java")
+    ruby=Topic.create(name: "ruby")
+    android=Topic.create(name:"android")
+    php=Topic.create(name:"php")
+
+
+
+
+    user = User.find(1).topics<<java
+    user = User.find(1).topics<<ruby
+    user = User.find(1).topics<<php
+
+    user = User.find(2).topics<<java
+
+    user = User.find(2).topics<<php
+   
