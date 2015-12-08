4cbc61356e56ecb494ec933a17b3bbe90141aec9
wenjin wu <wenjin@wuwenjindeMacBook-Pro.local>
2015-12-08 16:25:49 +0000

    chage some UI

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,6 +3,6 @@ class Topic < ActiveRecord::Base
 		      has_and_belongs_to_many :microposts
 		      has_many :askquestions
 
-  validates :name, presence: true
+  validates :name, presence: true,uniqueness: { case_sensitive: false }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,12 @@ has_many :answerquestions
       validates :name, length: { in: 4..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       validates :email,presence: true,
-       format: { with: VALID_EMAIL_REGEX }
+       format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+
+
+
+
       validates :password, presence: true, length: { minimum: 6 }  ,allow_blank: true
 
       validates :password_confirmation, presence: true,allow_blank: true
